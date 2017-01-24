927b1096c7aa3fde23d556b90b86c0b88d909fc2
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2017-01-24 15:53:38 +0200

    Started coding

--- Fastreader/src/main/java/pavelgarmuyev/fastreader/Main.java
+++ Fastreader/src/main/java/pavelgarmuyev/fastreader/Main.java
@@ -1,17 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pavelgarmuyev.fastreader;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author pavelgarmuyev
  */
+
 public class Main {
     
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input text below");
+        String input = scanner.nextLine();
+        
+        List words = new LinkedList<>();
+        
+        words.addAll(Arrays.asList(input.split(" ")));
+        
+        words.forEach(System.out::println);
     }
 }
