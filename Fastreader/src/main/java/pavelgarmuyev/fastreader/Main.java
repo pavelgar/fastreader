81ac3ba943f042292beae534fdeebd809dce3f4b
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2017-01-26 13:29:12 +0200

    Added code to Main.class

--- Fastreader/src/main/java/pavelgarmuyev/fastreader/Main.java
+++ Fastreader/src/main/java/pavelgarmuyev/fastreader/Main.java
@@ -1,6 +1,8 @@
 
 package pavelgarmuyev.fastreader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,16 +15,22 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input text below");
+        System.out.println("Copy plain text or give url");
         String input = scanner.nextLine();
         
         List words = new LinkedList<>();
         
         words.addAll(Arrays.asList(input.split(" ")));
+        System.out.println("When ready, press enter (type exit to terminate)");
+        String command = scanner.nextLine();
         
-        words.forEach(System.out::println);
+        if (!command.equals("exit")) {
+            words.forEach(System.out::println);
+        }
+        File file = new File("log.txt");
+        file.createNewFile();
     }
 }
